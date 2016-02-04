d18bee9841026525ecccb358b2dbfecc4a777e4c
Tom <tomw08@gmail.com>
2016-02-04 12:58:02 +0000

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import json
 import ObjectModelClasses as obj_class
 import inspect
 
-
 def get_id_list(length):
     return [x for x in range(0, length)]
 
@@ -26,27 +25,33 @@ def write_to_json(file_name, data):
     output_file.close()
 
 # test first with limited number of elements
-platform = {'currencies': obj_class.currencies,
+platform = {'categories' : obj_class.categories,
+            'currencies': obj_class.currencies,
             'countries': obj_class.countries,
             'languages': obj_class.languages,
             'platforms': obj_class.platforms,
+            'platform_type': obj_class.platform_types,
+            'platform_countries': obj_class.platform_countries,
+            'platform_language': obj_class.platform_languages,
+            'platform_offices': obj_class.platform_offices,
+            'platform_stat': obj_class.platform_stats,
+            'platform_descriptions': obj_class.platform_descriptions,
+            'platform_fee': obj_class.platform_fees,
+            'platform_statuses': obj_class.platform_statuses,
+            'platform_currencies': obj_class.platform_currencies,
+            'platform_contact': obj_class.platform_contacts,
+            'platform_categories_table': obj_class.platform_categories_table
+
             }
 # if test works then change these objects to dicts
 """           'world_bank_ranking': obj_class.WorldBankRanking(),
-            'platform_type': obj_class.Platform_Type(),
-            'platform_countries': obj_class.Platform_Countries(),
-            'platform_language': obj_class.Platform_Language(),
-            'platform_office': obj_class.Platform_Office(),
-            'platform_stat': obj_class.Platform_Stat(),
-            'platform_category': obj_class.Category(),
-            'platform_description': obj_class.Platform_Description(),
-            'platform_fee': obj_class.Platform_Fee(),
-            'social_media_site': obj_class.Social_Media_Site(),
-            'platform_status': obj_class.Platform_Status(),
-            'platform_currency': obj_class.Platform_Currency(),
-            'platform_contact': obj_class.Platform_Contact(),
-            'platform_categories_table': obj_class.Platform_Categories_Table()
+
+                        'social_media_site': obj_class.Social_Media_Site(),
+
             """
 
 # id_list = get_id_list(5)
+print(platform['platforms'].keys())
+print(platform['platforms'].values())
+
 
